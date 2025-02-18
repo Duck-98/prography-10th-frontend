0a0cf0eca6d98be24bd2748fd3b455cc41ece1d9
duck <godqhr2256@gmail.com>
2025-02-18 23:29:18 +0900

    feat 스탭 헤더 개발

--- src/components/Steps/StepHeader.tsx
+++ src/components/Steps/StepHeader.tsx
@@ -12,30 +12,28 @@ export const StepHeader: React.FC<StepHeaderProps> = ({
   totalSteps,
 }) => {
   return (
-    <div className="mb-8">
-      <div className="mb-4">
-        <div className="flex items-center w-full">
-          {[...Array(totalSteps)].map((_, index) => (
-            <div key={index} className="flex items-center flex-1">
-              <div
-                className={`w-8 h-8 rounded-full flex-shrink-0 flex items-center justify-center ${
-                  index + 1 <= currentStep
-                    ? "bg-blue-600 text-white"
-                    : "bg-gray-200 text-gray-600"
-                }`}
-              >
-                {index + 1}
-              </div>
-              {index < totalSteps - 1 && (
-                <div
-                  className={`h-1 w-full ${
-                    index + 1 < currentStep ? "bg-blue-600" : "bg-gray-200"
-                  }`}
-                />
-              )}
+    <div className="flex justify-center items-center py-4">
+      <div className="flex items-center w-fit">
+        {[...Array(totalSteps)].map((_, index) => (
+          <div key={index} className="flex items-center">
+            <div
+              className={`w-8 h-8 rounded-full flex items-center justify-center ${
+                index + 1 <= currentStep
+                  ? "bg-blue-600 text-white"
+                  : "bg-gray-200 text-gray-600"
+              }`}
+            >
+              {index + 1}
             </div>
-          ))}
-        </div>
+            {index < totalSteps - 1 && (
+              <div
+                className={`h-1 w-24 ${
+                  index + 1 < currentStep ? "bg-blue-600" : "bg-gray-200"
+                }`}
+              />
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
