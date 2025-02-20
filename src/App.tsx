43d2e8bb886c78b8fa3f06b11c2b4226f58b90f0
duck <godqhr2256@gmail.com>
2025-02-20 22:48:21 +0900

    refactor: suspense 로딩 컴포넌트 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Spinner } from "./components/Loading";
+import { FullPageLoading } from "./components/Loading";
 
 const RecruitmentPage = lazy(() => import("./pages/RecruitmentPage"));
 const ApplyPage = lazy(() => import("./pages/ApplyPage"));
-const ApplyCompletePage = lazy(() => import("./pages/ApplyCompletePage")); // 이렇게 변경
+const ApplyCompletePage = lazy(() => import("./pages/ApplyCompletePage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Spinner />}>
+      <Suspense fallback={<FullPageLoading />}>
         <Routes>
           <Route path="/" element={<RecruitmentPage />} />
           <Route path="/apply" element={<ApplyPage />} />
