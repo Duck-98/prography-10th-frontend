e55183f1d76bf2d8eb060d9e42c698843ea2f2bd
duck <godqhr2256@gmail.com>
2025-02-20 22:39:59 +0900

    style: 페이지 레이아웃 스타일링 수정

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -4,13 +4,21 @@
  * 페이지 레이아웃을 위한 컴포넌트
  */
 
+import { cn } from "@/utils/cn";
+
 interface PageLayoutProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const PageLayout = ({ children }: PageLayoutProps) => {
+const PageLayout = ({ children, className }: PageLayoutProps) => {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
+    <div
+      className={cn(
+        "flex flex-col justify-center items-center p-4 min-h-screen bg-gray-50",
+        className,
+      )}
+    >
       {children}
     </div>
   );
