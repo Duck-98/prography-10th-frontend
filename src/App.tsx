b18f61a6157d18b04d31c07718b38525cbeb0128
duck <godqhr2256@gmail.com>
2025-02-20 22:44:48 +0900

    feat: 404 페이지 개발 및 라우팅 연결

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Spinner } from "./components/Loading";
 const RecruitmentPage = lazy(() => import("./pages/RecruitmentPage"));
 const ApplyPage = lazy(() => import("./pages/ApplyPage"));
 const ApplyCompletePage = lazy(() => import("./pages/ApplyCompletePage")); // 이렇게 변경
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<RecruitmentPage />} />
           <Route path="/apply" element={<ApplyPage />} />
           <Route path="/apply/complete" element={<ApplyCompletePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h1 className="mb-4 text-4xl font-bold">404</h1>
+      <p className="mb-6 text-xl">페이지를 찾을 수 없습니다</p>
+      <Link to="/" className="text-blue-500 hover:text-blue-700">
+        메인페이지로 돌아가기
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
